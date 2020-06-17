32c715b6449cfce3b1c92f93d09e368223182929
jongyeonchae <jongyeonchae@gmail.com>
2020-06-18 00:07:25 +0900

    Express Server 생성

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hi");
+// [ express application 생성하기 ]
+// require: 내 파일 또는 폴더(node_modules) 내 "express" 이름을 가진 파일을 찾아 불러들임
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+// 4000 포트 에서 listen
+app.listen(PORT, handleListening);
