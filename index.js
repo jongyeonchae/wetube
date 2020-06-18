140dc645d955aa998b2b45062e1df6312f5e118a
jongyeonchae <jongyeonchae@gmail.com>
2020-06-18 11:55:18 +0900

    babel 적용

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 // [ express application로 서버 만들기 ]
 // require: 내 파일 또는 폴더(node_modules) 내 "express" 이름을 가진 파일을 찾아 불러들임
-const express = require("express");
+
+// babel 사용 위해 ES6 형식으로 JS 작성
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+// babel 사용 위해 ES6 형식으로 JS 작성
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
 
 // Request 와 Response 오브젝트 사용: 1)req: 어떤 정보를 가져올 것인가 2)res: 어떤 정보를 노출할 것인가
-function handleHome(req, res) {
-  console.log(req);
-  res.send(`Hello from home`);
-}
+const handleHome = (req, res) => res.send(`Hello from home`);
 
-function handleProfile(req, res) {
-  res.send(`You are on my profile`);
-}
+// babel 사용 위해 ES6 형식으로 JS 작성
+const handleProfile = (req, res) => res.send(`You are on my profile`);
 
 // [ route 만들기 ]
 // GET 메서드(정보 가져오기)를 통해 메인 URL 접근 시 handleHome 실행
