09b94e1c4c012c751317e59ac27b0ae794c98a9a
jongyeonchae <jongyeonchae@gmail.com>
2020-06-19 22:19:32 +0900

    MVC 적용

--- app.js
+++ app.js
@@ -14,16 +14,13 @@ import helmet from "helmet";
 
 import bodyParser from "body-parser";*/
 
-// default 아닌 오브젝트를 import 하기
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+
+import videoRouter from "./routers/videoRouter";
+
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-// Request 와 Response 오브젝트 사용: 1)req: 어떤 정보를 가져올 것인가 2)res: 어떤 정보를 노출할 것인가
-const handleHome = (req, res) => res.send(`Hello from home`);
-
-// babel 사용 위해 ES6 형식으로 JS 작성
-const handleProfile = (req, res) => res.send(`You are on my profile`);
 
 // [ helmet 전체 middleware로 적용하기 ]
 // helmet: 보안 담당.
@@ -39,19 +36,12 @@ app.use(morgan("dev"));
   res.send("not happening");
 }; */
 
-// [ route 만들기 ]
-// GET 메서드(정보 가져오기)를 통해 메인 URL 접근 시 handleHome 실행
-app.get("/", handleHome);
-// [ middleware를 메인 페이지에만 추가하기 ]
-/* app.get("/", betweenHome, handleHome); */
-// [ middleware로 connection 끊기 ]
-/* app.get("/", middlewareDisconnect, handleHome); */
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 // [ userRouter import 하기 ]
 // use: /user 에 누군가가 방문하면, userRouter 전체를 활용하겠다는 의미
 app.use("/user", userRouter);
+
+app.use("/video", videoRouter);
 
 // [app 오브젝트 export 하기]
 // 다른 파일에서 import 요청 시, app 오브젝트 import 허용
