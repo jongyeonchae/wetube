cf734de3d104e2c0e4f2fa0268c318c0fc9bb136
jongyeonchae <jongyeonchae@gmail.com>
2020-06-22 21:28:37 +0900

    pug 적용

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,4 @@
-export const home = (req, res) => res.send("Home");
+// [ pug 적용하기 ]
+export const home = (req, res) => res.render("home");
+
 export const search = (req, res) => res.send("Search");
