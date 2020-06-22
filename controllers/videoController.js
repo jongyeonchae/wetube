da0f8595b61b708ce082e8e28606bdbbcfc1b478
jongyeonchae <jongyeonchae@gmail.com>
2020-06-23 00:57:57 +0900

    특정 템플릿에 변수 적용

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
 // [ pug 적용하기 ]
-export const home = (req, res) => res.render("Home");
+// { pageTitle: "Home" }을 삽입하여 특정 템플릿에만 변수 적용
+export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
 
-export const search = (req, res) => res.render("Search");
+export const search = (req, res) =>
+  res.render("Search", { pageTitle: "Search" });
