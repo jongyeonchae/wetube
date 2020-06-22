c67aedd28648d0259cd657507856ad529a5c7497
jongyeonchae <jongyeonchae@gmail.com>
2020-06-22 13:10:11 +0900

    router 설정

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
 
 // ../: 현재 디렉토리(routers 폴더)를 벗어남을 의미
-import routes from "../routes";
+//import routes from "../routes";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, (req, res) => res.send("Home"));
+globalRouter.get(routes.search, (req, res) => res.send("Search"));
 globalRouter.get(routes.join, (req, res) => res.send("Join"));
 globalRouter.get(routes.login, (req, res) => res.send("Login"));
 globalRouter.get(routes.logout, (req, res) => res.send("Logout"));
-globalRouter.get(routes.search, (req, res) => res.send("Search"));
 
 export default globalRouter;
