885fd4426da6e1e0b80235869ad05da267ff5a93
jongyeonchae <jongyeonchae@gmail.com>
2020-06-22 21:28:17 +0900

    pug 설치

--- app.js
+++ app.js
@@ -14,16 +14,18 @@ import cookieParser from "cookie-parser";
 
 import bodyParser from "body-parser";
 
+import routes from "./routes";
+
 import userRouter from "./routers/userRouter";
 
 import videoRouter from "./routers/videoRouter";
 
 import globalRouter from "./routers/globalRouter";
 
-import routes from "./routes";
-
 const app = express();
 
+// view
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
