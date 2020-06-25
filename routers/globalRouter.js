a96b4748cb8d80de7ed826ed60493f0e609dce9f
jongyeonchae <jongyeonchae@gmail.com>
2020-06-25 16:57:23 +0900

    controller 적용

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,17 @@
 import express from "express";
 
 // ../: 현재 디렉토리(routers 폴더)를 벗어남을 의미
-//import routes from "../routes";
+import routes from "../routes";
+import { home, search } from "../controllers/videoController";
+import { join, login, logout } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.send("Home"));
-globalRouter.get(routes.search, (req, res) => res.send("Search"));
-globalRouter.get(routes.join, (req, res) => res.send("Join"));
-globalRouter.get(routes.login, (req, res) => res.send("Login"));
-globalRouter.get(routes.logout, (req, res) => res.send("Logout"));
+// [ Controller 파일 적용하기 ]
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
