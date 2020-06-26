a36ad9d18b8c43604e241eb6b900f122dbf54b29
jongyeonchae <jongyeonchae@gmail.com>
2020-06-26 14:17:41 +0900

    join controller 설정

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,26 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+// 로그인 이후 화면을 위해, postJoin 함수를 새로 생성
+export const postJoin = (req, res) => {
+  // 사용자가 입력한 키값(name, email 등)을 확인하고, 비밀번호가 일치 여부에 따라 다른 반응(res)
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    // status code(400: Bad Request)로 브라우저에게 비밀번호 일치여부 확인
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log User In
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Log in" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Log out" });
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -3,14 +3,21 @@ import express from "express";
 // ../: 현재 디렉토리(routers 폴더)를 벗어남을 의미
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { join, login, logout } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  login,
+  logout,
+} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 // [ Controller 파일 적용하기 ]
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
