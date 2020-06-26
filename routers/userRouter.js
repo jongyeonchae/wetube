06fae4b14262c93f0546b8af5a47b83041ddfc8d
jongyeonchae <jongyeonchae@gmail.com>
2020-06-26 11:52:02 +0900

    오류 수정

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,7 +11,8 @@ const userRouter = express.Router();
 
 // editProfile 호출 시 userDetail의 id로 인식하는 문제 해결위해, 위치 변경
 userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.userDetail, userDetail);
+// changePassword 호출 시 userDetail의 id로 인식하는 문제 해결위해, 위치 변경
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
