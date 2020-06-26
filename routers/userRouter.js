57af6a00ccfe9b5438f5dbff0be4b2c73940755c
jongyeonchae <jongyeonchae@gmail.com>
2020-06-26 15:13:10 +0900

    routes 수정
    user.id 를 활용하기 위해 함수 사용

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -13,6 +13,6 @@ const userRouter = express.Router();
 userRouter.get(routes.editProfile, editProfile);
 // changePassword 호출 시 userDetail의 id로 인식하는 문제 해결위해, 위치 변경
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
