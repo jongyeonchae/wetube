fbbeade9248456b2b7ac58d87c18157281741ed0
jongyeonchae <jongyeonchae@gmail.com>
2020-07-22 15:50:08 +0900

    editProfile 변수명 변경

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import routes from "../routes";
 import {
   userDetail,
-  editProfile,
+  getEditProfile,
   changePassword,
 } from "../controllers/userController";
 import { onlyPrivate } from "../middlewares";
@@ -10,7 +10,7 @@ import { onlyPrivate } from "../middlewares";
 const userRouter = express.Router();
 
 // editProfile 호출 시 userDetail의 id로 인식하는 문제 해결위해, 위치 변경
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 // changePassword 호출 시 userDetail의 id로 인식하는 문제 해결위해, 위치 변경
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
