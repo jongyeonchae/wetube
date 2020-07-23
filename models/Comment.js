c2f27366017b5550a4b4f9f1138144b018b220b8
jongyeonchae <jongyeonchae@gmail.com>
2020-07-23 18:34:10 +0900

    comment 내 creator 추가

--- models/Comment.js
+++ models/Comment.js
@@ -17,6 +17,10 @@ const CommentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Video",
   },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const model = mongoose.model("Comment", CommentSchema);
